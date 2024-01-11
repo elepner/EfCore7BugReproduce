9e1cdba3ade250224b9ee6de5a5ef4206ef46c1d
Eduard Lepner <eduard@invarion.com>
2024-01-11 10:43:30 +0100

    add initial migration

---
+++ EfCoreDockerCompose/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreDockerCompose;
+
+public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbContext(DbContextOptions<DbContext> options) : base(options)
+    {
+    }
+    public DbSet<SomeRecord> SomeRecords { get; set; }
+}
+
+public class SomeRecord
+{
+    public int Id { get; set; }
+    public string Descritpion { get; set; }
+}
